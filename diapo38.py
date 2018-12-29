db5cc9beb356dac69d740e863c47b98018fb087b
s3bc40 <leseb40@gmail.com>
2018-12-29 15:51:54 +0100

    add metabo : not working still

--- diapo38.py
+++ diapo38.py
@@ -15,27 +15,34 @@ print("Load JSON object ...")
 model = cobra.io.load_json_model("metacyc.json")
 print("Loading done.")
 
-
 for reaction in model.reactions:
-    if(reaction.subsystem == "PWY-7653"):
-        reacs.append(reaction)
+        if(reaction.subsystem == "PWY-7653"):
+                reacs.append(reaction)
 
+metabo_list = []
 for i in range(len(reacs)):
         for metabo in reacs[i].metabolites:
                 print metabo.name
                 a=0
                 for metabolite in modelEcoli.metabolites:
-                        if metabolite.name == metabo.name and a==0:
+                        if metabolite.name == metabo.name and a == 0:
                                 print "ID from ecoli model : ",metabolite.id
-                                a+=1
+                                a += 1
+                                print "Metabolites id : ",metabo.id
                                 metabo.id = metabolite.id
-                print "Metabolites id : ",metabo.id
+                        if metabo not in metabo_list and metabo not in modelEcoli.metabolites:
+                                metabo_list.append(metabo)
+                                
+                print "Metabolites change id : ",metabo.id
                 print "Value : ",reacs[i].metabolites[metabo]
-                print "\n"   
+                print "\n"
+
+print metabo_list
 
 print('Ce modèle contient %i réactions'% len(modelEcoli.reactions))
 print('Ce modèle contient %i métabolites'% len(modelEcoli.metabolites))
 print "Adding reactions and metabolites...\nDone\n"
+modelEcoli.add_metabolites(metabo_list)
 modelEcoli.add_reactions(reacs)
 print('Ce modèle contient %i réactions'% len(modelEcoli.reactions))
 print('Ce modèle contient %i métabolites'% len(modelEcoli.metabolites))
@@ -50,4 +57,3 @@ fluxMax = solution.objective_value
 print(fluxMax)
 
 modelEcoli.summary()
-
